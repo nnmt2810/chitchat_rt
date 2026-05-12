29dd142ecd3142ecd9dccbacae6f5f49b6acd6f7
23120102 <nnminhtuan2810@gmail.com>
2026-05-12 15:54:49 +0700

    online users implemented

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -1,9 +1,12 @@
 import { useEffect } from 'react';
 import { XIcon } from 'lucide-react';
 import { useChatStore } from '../store/useChatStore';
+import { useAuthStore } from '../store/useAuthStore';
 
 const ChatHeader = () => {
     const { selectedUser, setSelectedUser } = useChatStore();
+    const { onlineUsers } = useAuthStore();
+    const isOneline = onlineUsers.includes(selectedUser?._id);
 
     useEffect(() => {
         const handleEscKey = (e) => {
@@ -17,7 +20,7 @@ const ChatHeader = () => {
     return (
         <div className="flex justify-between items-center bg-slate-800/50 border-b border-slate-700/50 max-h-[74px] px-6 flex-1">
             <div className="flex items-center space-x-3">
-                <div className="avatar online">
+                <div className={`avatar ${isOneline ? "online" : "offline"}`}>
                     <div className="w-12 rounded-full"> 
                         <img src={selectedUser?.profilePic || '/avatar.png'} alt={selectedUser?.fullName} />
                     </div>
@@ -25,7 +28,7 @@ const ChatHeader = () => {
 
                 <div>
                     <h3 className="text-slate-200 font-medium">{selectedUser?.fullName}</h3>
-                    <p className="text-slate-400 text-sm">Online</p>
+                    <p className="text-slate-400 text-sm">{isOneline ? "Online" : "Offline"}</p>
                 </div>
             </div>
 
